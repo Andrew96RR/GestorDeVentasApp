193e4afe2eda6725cf9bae9da8e463ce616101c5
Andrew96RR <andresmrojasm@hotmail.com>
2025-11-03 17:03:21 -0500

    myNewCommit

--- GestorDeVentasApp/src/main/java/com/Hokkaido/GestorDeVentasApp/servicios/AssistansServicio.java
+++ GestorDeVentasApp/src/main/java/com/Hokkaido/GestorDeVentasApp/servicios/AssistansServicio.java
@@ -1,5 +1,10 @@
 package com.Hokkaido.GestorDeVentasApp.servicios;
 
+import java.util.List;
+
+import com.Hokkaido.GestorDeVentasApp.entidades.Assistants;
+
 public interface AssistansServicio {
+	List<Assistants> GetAllAssistants();
 
 }
--- GestorDeVentasApp/src/main/java/com/Hokkaido/GestorDeVentasApp/servicios/AssistansServicioImpl.java
+++ GestorDeVentasApp/src/main/java/com/Hokkaido/GestorDeVentasApp/servicios/AssistansServicioImpl.java
@@ -1,8 +1,23 @@
 package com.Hokkaido.GestorDeVentasApp.servicios;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.Hokkaido.GestorDeVentasApp.entidades.Assistants;
+import com.Hokkaido.GestorDeVentasApp.repositorios.AssistansRepositorio;
 
 @Service
 public class AssistansServicioImpl implements AssistansServicio {
+
+	@Autowired
+	private AssistansRepositorio assistansRepositorio ;
+	
+	@Override
+	public List<Assistants> GetAllAssistants() {
+		
+		return assistansRepositorio.findAll();
+	}
 
 }
